1a60b2ca6a9f359af36e9623df5eb6f31ac36810
Amit Sharma <awesomeamit50@gmail.com>
2020-08-16 01:46:36 +0530

    Added Firebase Auth, Real-time-database.

--- app/src/main/java/amit/action/besthindishayari/MainActivity.java
+++ app/src/main/java/amit/action/besthindishayari/MainActivity.java
@@ -2,7 +2,9 @@ package amit.action.besthindishayari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin(View view) {
+
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+    }
+
+    public void goToSignUp(View view) {
+        startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
     }
 }
