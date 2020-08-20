e77c54dd62479e8c7ac80e12c67c4b191b44e252
Amit Sharma <awesomeamit50@gmail.com>
2020-08-20 23:17:17 +0530

    Added Navigation View,New Layout for Main Activity.

--- app/src/main/java/amit/action/besthindishayari/MainActivity.java
+++ app/src/main/java/amit/action/besthindishayari/MainActivity.java
@@ -2,49 +2,54 @@ package amit.action.besthindishayari;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser curUser;
     private Button signUp,logoutBtn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
         mAuth=FirebaseAuth.getInstance();
         curUser=mAuth.getCurrentUser();
-        //signUp=findViewById(R.id.main_signup);
-        //logoutBtn=findViewById(R.id.main_logout);
-/*
 
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToSignUp();
-            }
-        });
-*/
-/*
+        List<String> list=new ArrayList<>();
+        list.add("Alone");
+        list.add("Sad");
+        list.add("Love");
+        list.add("Dosti");
+        list.add("Dard");
+        list.add("Good Morning");
+        list.add("Good Night");
+        list.add("Zindagi");
 
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                checkUserExistence();
-            }
-        });
-*/
 
     }
 
@@ -57,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void goToLogin(View view) {
 
-        //startActivity(new Intent(MainActivity.this,LoginActivity.class));
         Snackbar snackbar=Snackbar.make(view,"Welcome "+curUser.getDisplayName(),Snackbar.LENGTH_LONG);
         snackbar.show();
     }
@@ -72,5 +76,10 @@ public class MainActivity extends AppCompatActivity {
         if (curUser==null){
             goToSignUp();
         }
+    }
+
+    public void goToTopicMenu(View view) {
+        String id=view.getResources().getResourceEntryName(view.getId());
+        Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
     }
 }
