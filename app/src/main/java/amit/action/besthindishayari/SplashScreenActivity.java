1d8b32b3e31069cc1c090d3e154443ff58fc1a73
Amit Sharma <awesomeamit50@gmail.com>
2020-08-22 01:24:27 +0530

    Splash Screen Animation functioning Completed.

--- app/src/main/java/amit/action/besthindishayari/SplashScreenActivity.java
+++ app/src/main/java/amit/action/besthindishayari/SplashScreenActivity.java
@@ -2,13 +2,40 @@ package amit.action.besthindishayari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_TIMER=5000;
+    private ImageView appImage;
+    private TextView poweredName;
+    Animation sideAnim,bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        appImage=findViewById(R.id.splash_image);
+        poweredName=findViewById(R.id.splash_powered);
+
+        sideAnim= AnimationUtils.loadAnimation(this,R.anim.side_anim);
+        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+
+        appImage.setAnimation(sideAnim);
+        poweredName.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                finish();
+            }
+        },SPLASH_TIMER);
     }
 }
