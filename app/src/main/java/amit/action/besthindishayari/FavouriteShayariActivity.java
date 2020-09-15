84d3bc4b0092d148c98b2f587dd9c25dd88cdaa9
Amit Sharma <awesomeamit50@gmail.com>
2020-09-16 00:19:33 +0530

    Model class added, no fav shayari text display.

--- app/src/main/java/amit/action/besthindishayari/FavouriteShayariActivity.java
+++ app/src/main/java/amit/action/besthindishayari/FavouriteShayariActivity.java
@@ -1,9 +1,11 @@
 package amit.action.besthindishayari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,14 +14,21 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FavouriteShayariActivity extends AppCompatActivity {
     private TextView loginText,zeroShayariText;
     private Button loginBtn;
     private FirebaseAuth mAuth;
+    private DatabaseReference mRef;
     private FirebaseUser curUser;
     private RecyclerView recyclerView;
     private Toolbar toolbar;
+    private ProgressDialog mProgress;
 
 
     @Override
@@ -28,11 +37,17 @@ public class FavouriteShayariActivity extends AppCompatActivity {
         setContentView(R.layout.activity_favourite_shayari);
 
         mAuth=FirebaseAuth.getInstance();
+        curUser=mAuth.getCurrentUser();
 
         loginText=findViewById(R.id.fav_text_login);
         loginBtn=findViewById(R.id.fav_button_login);
         toolbar=findViewById(R.id.favourite_shayari_app_bar);
         zeroShayariText=findViewById(R.id.fav_shayari_text);
+        mProgress=new ProgressDialog(this);
+        mProgress.setTitle("Loading data...");
+        mProgress.setMessage("please wait");
+        mProgress.setCanceledOnTouchOutside(false);
+        mProgress.show();
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Favourite Shayaries");
@@ -53,12 +68,28 @@ public class FavouriteShayariActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        curUser=mAuth.getCurrentUser();
         if (curUser==null){
             loginText.setVisibility(View.VISIBLE);
             loginBtn.setVisibility(View.VISIBLE);
+            mProgress.dismiss();
         }else{
-            zeroShayariText.setVisibility(View.VISIBLE);
+
+            String uid=curUser.getUid();
+            mRef= FirebaseDatabase.getInstance().getReference().child(uid).child("favourite_shayari");
+            mRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (!dataSnapshot.exists()){
+                        zeroShayariText.setVisibility(View.VISIBLE);
+                    }
+                    mProgress.dismiss();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
         }
 
     }
@@ -66,5 +97,11 @@ public class FavouriteShayariActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(FavouriteShayariActivity.this,MainActivity.class));
+        finish();
     }
 }
--- app/src/main/java/amit/action/besthindishayari/MainActivity.java
+++ app/src/main/java/amit/action/besthindishayari/MainActivity.java
@@ -203,6 +203,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
     public void checkUserExistence(){
         curUser=mAuth.getCurrentUser();
         if (curUser==null){
